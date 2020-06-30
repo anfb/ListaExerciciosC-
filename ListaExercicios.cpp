19502f42b131f1d31993d1461c942d2ebaad8202
Adriely Barbosa <adriely.nara@gmail.com>
2020-06-30 08:43:46 -0300

    main

---
+++ ListaExercicios.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Q1.h"
+#include "Q2.h"
+#include "Q3.h"
+
+int main() 
+{
+	/*
+	std::cout << "\n Exercise 1 \n";
+	std::cout << "\n Amount Zeros found in the Matrix: " << RandomMatrix() << "\n";
+	std::cout << "--- End exercise 1 --- \n";
+
+
+	std::cout << "\n Exercise 2 \n";
+	int Number;
+
+	do {
+		std::cout << "Set a number greater than 0 and less than 1000: ";
+		std::cin >> Number;
+	} while (Number < 0 || Number>1000);
+
+	std::cout << "\n Sum result: " << DoubleSum(Number) << "\n";
+
+	std::cout << "--- End exercise 2 --- \n"; */
+
+	std::cout << "\n Exercise 3 \n";
+	FamousToys();
+	std::cout << "--- End exercise 3 --- \n";
+	system("PAUSE");
+	return 0;
+}
